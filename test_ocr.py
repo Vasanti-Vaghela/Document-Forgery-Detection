959f81c1b20fe6108821dfffb82b04570021f1ce
Neelam <neelamy9008@gmail.com>
2026-04-21 20:52:12 +0530

    daily progress

--- test_ocr.py
+++ test_ocr.py
@@ -3,8 +3,12 @@ from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-img = Image.open("text.png")
+img = Image.open("text.png.png")
+img2 = Image.open("text2.png.png")
+
 
 text = pytesseract.image_to_string(img)
+text2 = pytesseract.image_to_string(img2)
 
 print(text)
+print(text2)
